9afb7e931618908aeaa56d0b0a48a5e6932d691b
payevska <y.payevska@gmail.com>
2018-09-05 12:08:32 +0300

    Odliczanie czasu - first commit

---
+++ Odc_1/script.js
@@ -0,0 +1,9 @@
+var dzisiaj = new Date();
+
+var dzien = dzisiaj.getDate();
+var miesiac = dzisiaj.getMonth()+1;
+var rok = dzisiaj.getFullYear();
+
+var godzina = dzisiaj.Hours();
+var minuta = dzisiaj.Minutes();
+var secunda = dzidiaj.Seconds();
