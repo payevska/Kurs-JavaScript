7d08de9080f8c337b15237475ed3602ddc92155b
payevska <y.payevska@gmail.com>
2018-09-05 13:19:30 +0300

    add script.js

--- Odc_1/script.js
+++ Odc_1/script.js
@@ -1,9 +1,25 @@
-var dzisiaj = new Date();
+function odliczanie(){
+			var dzisiaj = new Date();
 
-var dzien = dzisiaj.getDate();
-var miesiac = dzisiaj.getMonth()+1;
-var rok = dzisiaj.getFullYear();
+			var dzien = dzisiaj.getDate();
+			if (dzien < 10) dzien = "0" + dzien;
 
-var godzina = dzisiaj.Hours();
-var minuta = dzisiaj.Minutes();
-var secunda = dzidiaj.Seconds();
+			var miesiac = dzisiaj.getMonth()+1;
+			if (miesiac < 10) miesiac = "0" + miesiac;
+
+			var rok = dzisiaj.getFullYear();
+
+			var godzina = dzisiaj.getHours();
+			if (godzina < 10) godzina = "0" + godzina;
+
+			var minuta = dzisiaj.getMinutes();
+			if (minuta < 10) minuta = "0" + minuta;
+
+			var secunda = dzisiaj.getSeconds();
+			if (secunda < 10) secunda = "0" + secunda;
+
+			document.getElementById("zegar").innerHTML = 
+				dzien + "/" + miesiac + "/" + rok + "|" + godzina + ":" + minuta + ":" + secunda;
+
+				setTimeout("odliczanie()",1000);
+		}
