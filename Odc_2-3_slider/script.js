4d54e803ef6be5973692e907ea8ec25d1d0cd908
payevska <y.payevska@gmail.com>
2018-09-06 17:13:43 +0300

    number of slider

--- Odc_2-3_slider/script.js
+++ Odc_2-3_slider/script.js
@@ -1,17 +1,31 @@
-var number = Math.floor(Math.random()*5) + 1;
+var numer = Math.floor(Math.random()*5) + 1;
+
+var timer1 = 0;
+var timer2 = 0;
+
+function ustawim(numslajdu) {
+	clearTimeout(timer1);
+	clearTimeout(timer2);
+	numer = numslajdu - 1;
+
+	schowaj();
+	setTimeout("zmienslajd()",500);
+	
+}
 
 function schowaj() {
 	$("#slider").fadeOut(500);
 }
 
 function zmienslajd() {
-	number++;
-	if (number > 5) number = 1;
-	var plik = "<img src=\"images/im" + number + ".png\">";
+	numer++;
+	if (numer > 5) numer = 1;
+	var plik = "<img src=\"images/im" + numer + ".png\">";
 
 	document.getElementById("slider").innerHTML = plik;
 	$("#slider").fadeIn(500);
-	setTimeout("zmienslajd()",5000);
-	setTimeout("schowaj()",4500);
+
+	timer1 = setTimeout("zmienslajd()",5000);
+	timer2 = setTimeout("schowaj()",4500);
 	
 };
