795ee52d55ca5c7e0600a5767a29814dca393e0a
payevska <y.payevska@gmail.com>
2018-09-06 15:56:08 +0300

    slider with jQuery

--- Odc_2-3_slider/script.js
+++ Odc_2-3_slider/script.js
@@ -1,12 +1,17 @@
 var number = Math.floor(Math.random()*5) + 1;
 
+function schowaj() {
+	$("#slider").fadeOut(500);
+}
+
 function zmienslajd() {
 	number++;
 	if (number > 5) number = 1;
 	var plik = "<img src=\"images/im" + number + ".png\">";
 
 	document.getElementById("slider").innerHTML = plik;
-
+	$("#slider").fadeIn(500);
 	setTimeout("zmienslajd()",5000);
+	setTimeout("schowaj()",4500);
 	
 };
